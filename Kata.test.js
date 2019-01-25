cd2e05c0915c4358f1140aa6ff0ec896b4cccf91
Kraiwich <le_kraiwich@hotmail.com>
2019-01-25 17:01:34 +0700

    finishing 5th case

---
+++ Kata.test.js
@@ -0,0 +1,28 @@
+const greet = require("./Kata")
+
+test("running method greet(\"Bob\") should return Hello, Bob.", ()=> {
+    expect(greet("Bob")).toBe("Hello, Bob.");
+});
+
+test("if input is null the return should be \"Hello, my friend.\"", ()=> {
+    expect(greet(null)).toBe("Hello, my friend.");
+});
+
+test("if the name is all uppercase like JERRY should return \"HELLO JERRY!\"", ()=> {
+    expect(greet("JERRY")).toBe("HELLO JERRY!");
+});
+
+test("given the array of two names [\"Jill\", \"Jane\"] should return \"Hello, Jill and Jane.\"", ()=> {
+    const givenArray = ["Jill", "Jane"];
+    expect(greet(givenArray)).toBe("Hello, Jill and Jane.");
+});
+
+test("given the array of n number of name (3 names) [\"Amy\", \"Brian\", \"Charlotte\"] should return \"Hello, Amy, Brian, and Charlotte.\"", ()=> {
+    const givenArray = ["Amy", "Brian", "Charlotte"];
+    expect(greet(givenArray)).toBe("Hello, Amy, Brian, and Charlotte.");
+});
+
+test("given the array mixed with uppercase name [\"Amy\", \"BRIAN\", \"Charlotte\"] should return \"Hello, Amy and Charlotte. AND HELLO BRIAN!\"",()=> {
+    const givenArray = ["Amy", "BRIAN", "Charlotte"];
+    expect(greet(givenArray)).toBe("Hello, Amy and Charlotte. AND HELLO BRIAN!");
+})
