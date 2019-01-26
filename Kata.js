a4b067df4cdc3e6231c206822cf4079487d46443
Kraiwich Kornsri <42442245+mikesmikey@users.noreply.github.com>
2019-01-26 09:13:28 +0700

    finishing case 6th

--- Kata.js
+++ Kata.js
@@ -4,37 +4,45 @@ function greet(name) {
     if (name === null || typeof(name) === "undefined") {
         return "Hello, my friend.";
     }
-    if (name.length === 2) {
-        return `Hello, ${name[0]} and ${name[1]}.`
-    } else if (name.length > 1) {
+    else if (typeof(name) === "object") {
+
         const normalName = name.filter((value) => { return value !== value.toUpperCase() });
-        const uppercaseName = name.filter((value) => { return value === value.toUpperCase() });
+        var normalStr = "";
 
-        if (normalName.length > 0 && uppercaseName.length > 0) {
-            var lastName = normalName[name.length-1];
+        if (normalName.length === 1) {
+            normalStr = `Hello, ${normalName[0]}.`;
+        } else if (normalName.length === 2){
+            normalStr = `Hello, ${normalName[0]} and ${normalName[1]}.`;
+        } else if (normalName.length !== 0) {
+            const lastName = normalName[normalName.length-1];
             delete normalName[normalName.length-1];
-            var nameStr = normalName.join(", ");
+            const nameStr = normalName.join(", ");
 
-            const normalStr = `Hello, ${nameStr}and ${lastName}.`
-
-            lastName = uppercaseName[name.length-1];
-
-            if (uppercaseName.length > 1) {
-                delete uppercaseName[uppercaseName.length-1];
-            }
-            nameStr = uppercaseName.join(", ");
-
-            const upperStr = `Hello, ${nameStr}and ${lastName}.`
-
-            return normalStr + upperStr;
-
-        } else if (normalName.length > 0) {
-
-        }  else if (uppercaseName.length > 0) {
-
+            normalStr = `Hello, ${nameStr}and ${lastName}.`
         }
 
-        return `Hello, ${nameStr}and ${lastName}.`;
+        const uppercaseName = name.filter((value) => { return value === value.toUpperCase() });
+        var upperStr = "";
+
+        if (uppercaseName.length === 1) {
+            upperStr = `HELLO ${uppercaseName[0]}!`;
+        } else if (uppercaseName.length === 2) {
+            upperStr = `HELLO ${uppercaseName[0]} AND ${uppercaseName[1]}.`;
+        } else if (uppercaseName.length !== 0) {
+            const lastName = uppercaseName[uppercaseName.length-1];
+            delete uppercaseName[uppercaseName.length-1];
+            const nameStr = uppercaseName.join(", ");
+
+            upperStr = `HELLO ${nameStr}AND ${lastName}!`;
+        }
+
+        if (normalStr !== "" && upperStr !== "") {
+            return `${normalStr} AND ${upperStr}`;
+        } else if (normalStr !== "") {
+            return normalStr;
+        } else if (upperStr !== "") {
+            return upperStr
+        }
     } else if (name.toUpperCase() === name) {
         return `HELLO ${name}!`
     }
